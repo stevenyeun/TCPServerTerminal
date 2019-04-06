92a1012f51053b288ab23e402d931b1578d6b38c
stevenYeun <mryeun2@gmail.com>
2019-04-06 15:25:24 +0900

    코드 수정

---
+++ TCPServerTerminal/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServerTerminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // (1) 로컬 포트 7000 을 Listen
+            TcpListener listener = new TcpListener(IPAddress.Loopback, 3000);
+            listener.Start();
+
+            byte[] buff = new byte[1024];
+
+            while (true)
+            {
+                // (2) TcpClient Connection 요청을 받아들여
+                //     서버에서 새 TcpClient 객체를 생성하여 리턴
+                TcpClient tc = listener.AcceptTcpClient();
+                Console.WriteLine("AcceptTcpClient");
+
+                // (3) TcpClient 객체에서 NetworkStream을 얻어옴 
+                NetworkStream stream = tc.GetStream();
+
+                // (4) 클라이언트가 연결을 끊을 때까지 데이타 수신
+                int nbytes;
+                try
+                {
+                    while ((nbytes = stream.Read(buff, 0, buff.Length)) > 0)
+                    {
+                        System.Console.WriteLine(buff);
+                        // (5) 데이타 그대로 송신
+                        stream.Write(buff, 0, nbytes);
+                    }
+                }
+                catch(Exception e)
+                {
+
+                }
+
+                // (6) 스트림과 TcpClient 객체 
+                stream.Close();
+                tc.Close();
+
+                // (7) 계속 반복
+            }
+        }
+    }
+}
